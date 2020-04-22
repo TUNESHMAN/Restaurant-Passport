d6fdce09d853b531dc69af00f04dadc962e55f71
Babatunde <babatundea15@gmail.com>
2020-04-22 14:57:00 +0100

    refactored register and login to antd

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,56 +1,107 @@
 import React, { useState } from "react";
+import "antd/dist/antd.css";
+import { Form, Icon, Input, Button, message, Spin } from "antd";
+import "./form.css";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
-function SignIn(props) {
-  const [details, setDetails] = useState({
-    username: "",
-    password: ""
-  });
-
-  const handleDetails = e => {
-    setDetails({
-      ...details,
-      [e.target.name]: e.target.value
+const SignIn = (props) => {
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    props.form.validateFieldsAndScroll((error, values) => {
+      const loginDetails = {
+        username: values.username,
+        password: values.password,
+      };
+      if (!error) {
+        message.loading(`You are logging in...`, 1.5);
+        axios
+          .post(
+            `https://build-restaurant-passport.herokuapp.com/users/login`,
+            loginDetails
+          )
+          .then((res) => {
+            setLoading(false);
+            message.success(`You are logged in successfully`, 1.2);
+            localStorage.setItem(`token`, res.token);
+            props.history.push("/cities");
+          })
+          .catch((error) => {
+            setLoading(false);
+            message.error(
+              `Unsuccessful login attempt, please register or try again!`,
+              2.0
+            );
+          });
+      } else {
+      }
     });
   };
-  const handleFormSubmit = e => {
-    e.preventDefault();
-    // make a POST request to the server
-    // the server will "authenticate" the user passed on their credentials
-    // If they can be authenticated the server will return a token
-    axios
-      .post("https://build-restaurant-passport.herokuapp.com/users/login")
-      .then(res => {
-        localStorage.setItem("token", res.data.payload);
-        props.history.push('/cities')
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+  const { getFieldDecorator } = props.form;
   return (
-    <div>
-      <form onSubmit={handleFormSubmit}>
-        <label>USERNAME</label>
-        <input
-          type="text"
-          placeholder="username"
-          name="username"
-          onChange={handleDetails}
-        />
-
-        <label>PASSWORD</label>
-        <input
-          type="password"
-          placeholder="Enter Password"
-          name="password"
-          onChange={handleDetails}
-        />
-
-        <input type="submit" />
-      </form>
+    <div className="form-div">
+      <Spin spinning={loading}>
+        <Form onSubmit={handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator("username", {
+              //rules are for the form validation
+              rules: [
+                { required: true, message: "Please input a username!" },
+                {
+                  type: "string",
+                  message: "Username is not correct",
+                },
+              ],
+            })(
+              <Input
+                name="username"
+                //form icon in the email field, change type for different icons, see antdesign docs
+                prefix={
+                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder="Username"
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator("password", {
+              //rules are for the form validation
+              rules: [
+                { required: true, message: "Please input a password!" },
+                {
+                  type: "string",
+                  message: "Invalid password",
+                },
+              ],
+            })(
+              <Input
+                name="password"
+                type="password"
+                //form icon in the email field, change type for different icons, see antdesign docs
+                prefix={
+                  <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder="Password"
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Login
+            </Button>
+            Don't have an account?
+            <Link to="/register"> Sign-up here!</Link>
+          </Form.Item>
+        </Form>
+      </Spin>
     </div>
   );
-}
-
-export default SignIn;
+};
+const WrappedNormalLoginForm = Form.create({ name: "normal_login" })(SignIn);
+export default WrappedNormalLoginForm;
