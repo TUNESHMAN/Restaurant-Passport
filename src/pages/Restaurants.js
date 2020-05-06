a9a09be373b44d42ec6ccd8de25b76507dbfa28a
Babatunde <babatundea15@gmail.com>
2020-05-06 16:18:19 +0100

    i styled the restaurants page

--- src/pages/Restaurants.js
+++ src/pages/Restaurants.js
@@ -1,47 +1,65 @@
-import React, { useEffect } from "react";
-import NavBar from "../components/NavBar";
+import React from "react";
 import { getRestaurant } from "../state/Actions/cityAction";
 import { connect } from "react-redux";
-import { Spin, Card } from "antd";
+import { Spin, Card, Menu, Icon } from "antd";
+// import SubMenu from "antd/lib/menu/SubMenu";
+import { NavLink } from "react-router-dom";
+import "./rest.css";
 
 function Restaurants(props) {
   return (
-    <div>
-      <NavBar />
-      <div>
+    <div className="rest-side">
+      <Menu>
+        <Menu.Item key="home">
+          <NavLink to="/cities" className="back-nav">
+            <Icon type="left" />
+            <span className="back">Back to Cities</span>
+          </NavLink>
+        </Menu.Item>
+      </Menu>
+      <div className="no-rest">
         {!props.restaurantInfo.restaurants && !props.fetching && (
-          <h2>No restaurant in this city</h2>
+          <h2 className="zero-rest">No restaurant in this city</h2>
         )}
         {props.fetching && (
-          <div>
-            <Spin spinning={props.fetching} />
+          <div className="loader">
+            <Spin spinning={props.fetching} style={{ color: "white" }} />
           </div>
         )}
         {props.restaurantInfo.restaurants && !props.fetching && (
           <div>
             {props.restaurantInfo.restaurants && !props.fetching && (
-              <div>
-                <h2>
-                  These are the restaurants in {props.restaurantInfo.name}
-                </h2>
+              <div className="rest-card">
                 {props.restaurantInfo.restaurants.map((restaurant) => (
-                  <div className="card-wrapper">
+                  <div className="rest-wrapper">
                     <Card
-                      title="Card title"
-                      bordered={false}
-                      style={{ width: 300 }}
+                      //   bordered={false}
+                      style={{
+                        width: 300,
+                        marginTop: "12px",
+                        marginLeft: "15px",
+                        marginRight: "15px",
+                        marginBottom: "12px",
+                      }}
                     >
                       <p>
-                        <span>Restaurant Name:</span>
-                        <span>{restaurant.restName}</span>
+                        <span className="rest-name">Restaurant Name:</span>
+                        <span className="rest-add"> {restaurant.restName}</span>
                       </p>
                       <p>
-                        <span>Address of Restaurant:</span>
-                        <span>{restaurant.restAddress}</span>
+                        <span className="rest-name">
+                          Address of Restaurant:
+                        </span>
+                        <span className="rest-add">
+                          {" "}
+                          {restaurant.restAddress}
+                        </span>
                       </p>
                       <p>
-                        <span>Description of Restaurant</span>
-                        <span>{restaurant.restDesc}</span>
+                        <span className="rest-name">
+                          Description of Restaurant
+                        </span>
+                        <span className="rest-add"> {restaurant.restDesc}</span>
                       </p>
                     </Card>
                   </div>
